9613738471f5d90c34ed1cd2df7c35fe3f33774e
fakedann <daniel07escalona@gmail.com>
2024-08-16 11:14:54 -0400

    container testing

--- Desktop/inspections/inspection-project/src/App.js
+++ Desktop/inspections/inspection-project/src/App.js
@@ -1,12 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, createContext} from 'react';
+import FrontPage from './FrontPage';
+
+export const globalContext = createContext();
 
 function App() {
+  const [language, setLanguage] = useState('English')
+  const languages = {language, setLanguage}
+
   return (
-    <div className="App">
-      <p>hola qlq</p>
-      
-    </div>
+    <globalContext.Provider value={languages}>
+        <div className="App">
+          <p>hola qlq</p>
+          <FrontPage />
+        </div>
+    </globalContext.Provider>
+  
   );
 }
 
--- Desktop/inspections/inspection-project/src/FrontPage.js
+++ Desktop/inspections/inspection-project/src/FrontPage.js
@@ -2,11 +2,24 @@ import { useContext } from "react";
 import { globalContext } from "./App"
 
 function FrontPage(){
+    const {language, setLanguage} = useContext(globalContext)
 
     return (
         <div>
             <p>frontpage</p>
-            <p>testing commits here</p>
+            <p>{language}</p>
+            <button onClick={ () => setLanguage('Espa')}>CHange language</button>
+            <div className="container">
+                <button>BOX 1</button>
+                <button>BOX 2</button>
+                <button>BOX 3</button>
+                <button>BOX 4</button>
+                <button>BOX 5</button>
+                <button>BOX 6</button>
+                <button>BOX 7</button>
+                <button>BOX 7</button>
+
+            </div>
         </div>
     )
 }
