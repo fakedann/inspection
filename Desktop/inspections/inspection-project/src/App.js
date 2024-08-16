eab2d81356ea8cfe52892ac64cd7f1cbbd7eac10
fakedann <daniel07escalona@gmail.com>
2024-08-16 10:23:16 -0400

    first commit

---
+++ Desktop/inspections/inspection-project/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>hola qlq</p>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ Desktop/inspections/inspection-project/src/FrontPage.js
@@ -0,0 +1,13 @@
+import { useContext } from "react";
+import { globalContext } from "./App"
+
+function FrontPage(){
+
+    return (
+        <div>
+            <p>frontpage</p>
+        </div>
+    )
+}
+
+export default FrontPage;
